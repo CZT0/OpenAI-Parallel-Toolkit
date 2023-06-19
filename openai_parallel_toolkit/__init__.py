da67f0839ebb9102cacc42ef8c061df978eca5f0
Jellow <dvdx@foxmail.com>
2023-06-19 19:22:13 +0800

    Removed the token counting feature, which has little to do with this project Built-in GPT-3.5 16k, GPT-3.5 0613, GPT-4 are provided

--- openai_parallel_toolkit/__init__.py
+++ openai_parallel_toolkit/__init__.py
@@ -1,3 +1,2 @@
-from .api import request_openai_api, OpenAIModel, Gpt35Turbo, APIKeyManager
-from .core import ParallelToolkit, multi_thread_run, multi_process_one
-from .utils import num_tokens_from_string, split_string_by_tokens
+from .api import APIKeyManager, Gpt35Turbo, OpenAIModel, request_openai_api
+from .core import ParallelToolkit, multi_process_one, multi_thread_run
--- openai_parallel_toolkit/utils/__init__.py
+++ openai_parallel_toolkit/utils/__init__.py
@@ -1,4 +1,3 @@
 from .logger import logger_init
-from .reader import read_keys, read_api_base, partition_data, read_folder, read_files
-from .token import num_tokens_from_string, split_string_by_tokens
+from .reader import partition_data, read_api_base, read_files, read_folder, read_keys
 from .tqdm import ProgressBar
